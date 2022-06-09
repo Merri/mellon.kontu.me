99eaa83655160f7236a928e013e1179678477a0c
Vesa Piittinen <vesa.piittinen@gmail.com>
2022-06-09 19:56:22 +0300

    Improve meetup date handling, nicer front page

---
+++ src/lib/meetup.ts
@@ -0,0 +1,67 @@
+import { isBefore, endOfDay, isSameDay, addMinutes, isSameYear } from 'date-fns'
+import { format as tzf } from 'date-fns-tz'
+import locale from 'date-fns/locale/fi/index.js'
+
+import type { Meetup } from '$/types/db'
+
+interface MeetupDateOptions {
+	format: string
+	now: Date
+	meetup?: Meetup
+}
+
+const opts = { locale, timeZone: 'Europe/Helsinki' }
+
+/** Get registration closing date */
+export function getRegistrationCloseDate(meetup?: Meetup): Date | null {
+	if (!meetup) return null
+	return meetup.begin
+		? meetup.end
+			? isSameDay(meetup.begin, meetup.end)
+				? addMinutes(meetup.begin, -5)
+				: endOfDay(meetup.begin)
+			: meetup.begin
+		: null
+}
+
+/** Get registration closing date as string if it is in the future */
+export function getTextRegistrationClose({ format, now, meetup }: MeetupDateOptions): string | null {
+	const closeDate = getRegistrationCloseDate(meetup)
+	if (!closeDate || isBefore(closeDate, now)) return null
+	return tzf(closeDate, format, opts)
+}
+
+/** `EEEE d.M.Y 'klo' H:mm` */
+
+/** Get registration opening date as string if it is in the future */
+export function getTextRegistrationOpen({ format, now, meetup }: MeetupDateOptions): string | null {
+	if (!meetup?.open || isBefore(meetup.open, now)) return null
+	return tzf(meetup.open, format, opts)
+}
+
+export function getTextMeetupDateRange(meetup?: Meetup): string | null {
+	if (!meetup) return null
+	if (meetup.begin == null) {
+		if (meetup.end == null) return 'Ajankohta ei tiedossa'
+		return tzf(meetup.end, `'Päättyy' EEEEEE d.M.Y 'klo' H:mm (alkamisaika ei tiedossa)`, opts)
+	}
+
+	if (meetup.end == null) {
+		return tzf(meetup.begin, `'Alkaa' EEEEEE d.M.Y 'klo' H:mm '(päättymisaika ei tiedossa)'`, opts)
+	}
+
+	if (isSameDay(meetup.begin, meetup.end)) {
+		return tzf(meetup.begin, `EEEEEE d.M.Y 'klo' H:mm '\u2013' `, opts) + tzf(meetup.end, `H:mm`, opts)
+	}
+
+	if (isSameYear(meetup.begin, meetup.end)) {
+		return (
+			tzf(meetup.begin, `EEEEEE d.M. 'klo' H:mm '\u2013' `, opts) +
+			tzf(meetup.end, `EEEEEE d.M.Y 'klo' H:mm`, opts)
+		)
+	}
+
+	return (
+		tzf(meetup.begin, `EEEEEE d.M.Y 'klo' H:mm '\u2013' `, opts) + tzf(meetup.end, `EEEEEE d.M.Y 'klo' H:mm`, opts)
+	)
+}
